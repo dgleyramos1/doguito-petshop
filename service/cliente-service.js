054fec95e1715b063905d315a254f934c8f35d13
Dgley Ramos <dgleysilva@gmail.com>
2022-10-25 15:41:21 -0300

    refatorando responsabilidades

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,36 +1,12 @@
-const criaNovaLinha = (nome, email) => {
-  const linhaNovoCliente = document.createElement('tr');
-  const conteudo = `
-      <td class="td" data-td>${nome}</td>
-        <td>${email}</td>
-        <td>
-          <ul class="tabela__botoes-controle">
-            <li>
-              <a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a>
-            </li>
-            <li>
-              <button class="botao-simples botao-simples--excluir" type="button">Excluir</button>
-            </li>
-          </ul>
-        </td>`
-
-  linhaNovoCliente.innerHTML = conteudo
-  return linhaNovoCliente;
-}
-
-
-const tabela = document.querySelector('[data-tabela]');
-
 const listaClientes = () => {
   return fetch(`http://localhost:3000/profile`)
           .then(response => {
             return response.json();
           })
 }
-      
-listaClientes()
-  .then(data => {
-    data.forEach(element => {
-      tabela.appendChild(criaNovaLinha(element.nome, element.email))
-    })
-  })
+
+
+
+export const clienteService = {
+  listaClientes
+}
