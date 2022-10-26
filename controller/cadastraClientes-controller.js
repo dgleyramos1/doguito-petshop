bc55fa1c80f71f35fa2a0e016f78612cdc197da9
Dgley Ramos <dgleysilva@gmail.com>
2022-10-26 15:44:36 -0300

    Finish

--- controller/cadastraClientes-controller.js
+++ controller/cadastraClientes-controller.js
@@ -3,7 +3,7 @@ import {clienteService} from '../service/cliente-service.js';
 const formulario = document.querySelector('[data-form]');
 
 
-formulario.addEventListener('submit', (e) => {
+formulario.addEventListener('submit', async (e) => {
   e.preventDefault()
   const nome = e.target.querySelector('[data-nome]').value;
   const email = e.target.querySelector('[data-email]').value;
